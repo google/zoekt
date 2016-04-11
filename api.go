4a223ac68ad6bcbc10c1488ddc7862f322d001e6
Han-Wen Nienhuys <hanwen@google.com>
2016-04-11 20:55:03 +0200

    Add missing file.

---
+++ api.go
@@ -0,0 +1,46 @@
+package codesearch
+
+import (
+	"time"
+)
+
+type FileMatch struct {
+	// Ranking; the lower, the better.
+	Rank    int
+	Name    string
+	Matches []Match
+}
+
+type Match struct {
+	Line    string
+	LineNum int
+	LineOff int
+
+	Offset      uint32
+	MatchLength int
+}
+
+type Stats struct {
+	NgramMatches int
+	FilesLoaded  int
+	FileCount    int
+	MatchCount   int
+	Duration     time.Duration
+}
+
+func (s *Stats) Add(o Stats) {
+	s.NgramMatches += o.NgramMatches
+	s.FilesLoaded += o.FilesLoaded
+	s.MatchCount += o.MatchCount
+	s.FileCount += o.FileCount
+}
+
+type SearchResult struct {
+	Stats
+	Files []FileMatch
+}
+
+type Searcher interface {
+	Search(query Query) (*SearchResult, error)
+	Close() error
+}
