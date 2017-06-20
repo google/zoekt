e20e7193156aebaff2c28c0598bd47f2066eba14
Han-Wen Nienhuys <hanwen@google.com>
2017-06-20 17:37:06 +0200

    Revert "Load shards as fast as we can"
    The current strategy has an unexplained deadlock, which apparently
    triggers when read requests happen concurrently with reloads.
    
    Since the GCP load balancer issues a continuing stream of HTTP
    requests for /, each of which triggers a read across shards, the
    deadlock trigger in production reliably.
    
    This reverts commit e9c19a019ce90ce9ce7021c81c8d0bb10aedeaed.
    
    Change-Id: I5b5234d204a9e9303ab84b098443dd9e9709318f

--- shards/shards.go
+++ shards/shards.go
@@ -15,7 +15,10 @@
 package shards
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 	"runtime"
 	"runtime/debug"
 	"sort"
@@ -23,14 +26,199 @@ import (
 
 	"golang.org/x/net/context"
 
+	"github.com/fsnotify/fsnotify"
 	"github.com/google/zoekt"
 	"github.com/google/zoekt/query"
 )
 
+type searchShard struct {
+	zoekt.Searcher
+	mtime time.Time
+}
+
+type shardWatcher struct {
+	dir string
+
+	// Limit the number of parallel queries. Since searching is
+	// CPU bound, we can't do better than #CPU queries in
+	// parallel.  If we do so, we just create more memory
+	// pressure.
+	throttle chan struct{}
+
+	shards map[string]*searchShard
+	quit   chan struct{}
+}
+
+func loadShard(fn string) (*searchShard, error) {
+	f, err := os.Open(fn)
+	if err != nil {
+		return nil, err
+	}
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	iFile, err := zoekt.NewIndexFile(f)
+	if err != nil {
+		return nil, err
+	}
+	s, err := zoekt.NewSearcher(iFile)
+	if err != nil {
+		iFile.Close()
+		return nil, fmt.Errorf("NewSearcher(%s): %v", fn, err)
+	}
+
+	return &searchShard{
+		mtime:    fi.ModTime(),
+		Searcher: s,
+	}, nil
+}
+
+func (s *shardWatcher) String() string {
+	return fmt.Sprintf("shardWatcher(%s)", s.dir)
+}
+
+func (s *shardWatcher) scan() error {
+	fs, err := filepath.Glob(filepath.Join(s.dir, "*.zoekt"))
+	if err != nil {
+		return err
+	}
+
+	if len(fs) == 0 {
+		return fmt.Errorf("directory %s is empty", s.dir)
+	}
+
+	ts := map[string]time.Time{}
+	for _, fn := range fs {
+		key := filepath.Base(fn)
+		fi, err := os.Lstat(fn)
+		if err != nil {
+			continue
+		}
+
+		ts[key] = fi.ModTime()
+	}
+
+	s.lock()
+	var toLoad []string
+	for k, mtime := range ts {
+		if s.shards[k] == nil || s.shards[k].mtime != mtime {
+			toLoad = append(toLoad, k)
+		}
+	}
+
+	var toDrop []string
+	// Unload deleted shards.
+	for k := range s.shards {
+		if _, ok := ts[k]; !ok {
+			toDrop = append(toDrop, k)
+		}
+	}
+	s.unlock()
+
+	for _, t := range toDrop {
+		log.Printf("unloading: %s", t)
+		s.replace(t, nil)
+	}
+
+	for _, t := range toLoad {
+		shard, err := loadShard(filepath.Join(s.dir, t))
+		log.Printf("reloading: %s, err %v ", t, err)
+		if err != nil {
+			continue
+		}
+		s.replace(t, shard)
+	}
+
+	return nil
+}
+
+func (s *shardWatcher) rlock() {
+	s.throttle <- struct{}{}
+}
+
+// getShards returns the currently loaded shards. The shards must be
+// accessed under a rlock call.
+func (s *shardWatcher) getShards() []zoekt.Searcher {
+	var res []zoekt.Searcher
+	for _, sh := range s.shards {
+		res = append(res, sh)
+	}
+	return res
+}
+
+func (s *shardWatcher) runlock() {
+	<-s.throttle
+}
+
+func (s *shardWatcher) lock() {
+	n := cap(s.throttle)
+	for n > 0 {
+		s.throttle <- struct{}{}
+		n--
+	}
+}
+
+func (s *shardWatcher) unlock() {
+	n := cap(s.throttle)
+	for n > 0 {
+		<-s.throttle
+		n--
+	}
+}
+
+func (s *shardWatcher) replace(key string, shard *searchShard) {
+	s.lock()
+	defer s.unlock()
+	old := s.shards[key]
+	if old != nil {
+		old.Close()
+	}
+	if shard != nil {
+		s.shards[key] = shard
+	} else {
+		delete(s.shards, key)
+	}
+}
+
+func (s *shardWatcher) watch() error {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return err
+	}
+	if err := watcher.Add(s.dir); err != nil {
+		return err
+	}
+
+	go func() {
+		for {
+			select {
+			case <-watcher.Events:
+				s.scan()
+			case err := <-watcher.Errors:
+				if err != nil {
+					log.Println("watcher error:", err)
+				}
+			case <-s.quit:
+				watcher.Close()
+				return
+			}
+		}
+	}()
+	return nil
+}
+
 // NewShardedSearcher returns a searcher instance that loads all
 // shards corresponding to a glob into memory.
 func NewShardedSearcher(dir string) (zoekt.Searcher, error) {
-	ss := newShardWatcher(dir)
+	ss := shardWatcher{
+		dir:      dir,
+		shards:   make(map[string]*searchShard),
+		quit:     make(chan struct{}, 1),
+		throttle: make(chan struct{}, runtime.NumCPU()),
+	}
+
 	if err := ss.scan(); err != nil {
 		return nil, err
 	}
@@ -39,7 +227,7 @@ func NewShardedSearcher(dir string) (zoekt.Searcher, error) {
 		return nil, err
 	}
 
-	return &shardedSearcher{ss}, nil
+	return &shardedSearcher{&ss}, nil
 }
 
 // Close closes references to open files. It may be called only once.
