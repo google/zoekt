f879d6c17f6674c8b691950d7e159f388709338d
Gustaf Lundh <gustaflh@axis.com>
2017-10-25 11:56:01 +0200

    Polish and bootstrapify UI
    Screen shots:
    -------------
    
    * Landing page:
    https://i.imgur.com/Hz7yFhv.png
    
    * Search result:
    https://i.imgur.com/uOmDERp.png
    https://i.imgur.com/OGF47w9.png (navbar follows)
    
    * Repo list:
    https://i.imgur.com/IJNgFj1.png
    
    Notable changes:
    ----------------
    
    * Bootstrap 3.3 components and CSS used.
    
    * UI is responsive to smaller screen sizes and should work
    better with mobile devices.
    
    * Max hits are only possible to refine on the search result
    screen, since users typically do not specify number of wanted
    hits until they have seen how many results are initially
    returned.
    
    * The navbar/searchbox follows the scroll, since users are
    more likely to want to refine their searches after scrolling
    through a few pages.
    
    * Meta-data not important for end-users (ngram & shard stats) has
    been moved out of the way (typically to the page footer).
    
    Change-Id: Iccb8bff3fd24eefa000204c38297323b571cc6fa

--- web/templates.go
+++ web/templates.go
@@ -25,164 +25,235 @@ var Top = template.New("top").Funcs(Funcmap)
 // TemplateText contains the text of the standard templates.
 var TemplateText = map[string]string{
 
-	"didyoumean": `<html>
-  <head>
-    <title>Error</title>
-  </head>
-  <body>
-    <p>{{.Message}}. Did you mean <a href="/search?q={{.Suggestion}}">{{.Suggestion}}</a> ?
-  </body>
+	"didyoumean": `
+<html>
+<head>
+  <title>Error</title>
+</head>
+<body>
+  <p>{{.Message}}. Did you mean <a href="/search?q={{.Suggestion}}">{{.Suggestion}}</a> ?
+</body>
 </html>
+`,
+
+	"head": `
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE) -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<style>
+  #navsearchbox { width: 350px !important; }
+  #maxhits { width: 100px !important; }
+  #results { padding-top: 60px; }
+  .inline-pre { border: unset; background-color: unset; margin: unset; padding: unset; }
+  table tbody tr td { border: none !important; padding: 2px !important; }
+</style>
+</head>
+  `,
+
+	"jsdep": `
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 `,
 
 	// the template for the search box.
 	"searchbox": `
-  <form action="search">
-    Search some code: <input
-      {{if .AutoFocus}}
-      autofocus
-      onfocus="this.value = this.value;"
-      {{end}}
-      {{if .Query}}value={{.Query}}
-      {{end}}type="text" name="q"> Max results:  <input style="width: 5em;" type="text" name="num" value="{{.Num}}"> <input type="submit" value="Search">
-  </form>
+<form action="search">
+  <div class="form-group form-group-lg">
+    <div class="input-group input-group-lg">
+      <input class="form-control" placeholder="Search for some code..."
+              id="searchbox" type="text" name="q">
+      <div class="input-group-btn">
+        <button class="btn btn-primary">Search</button>
+      </div>
+    </div>
+  </div>
+</form>
 `,
 
+	"navbar": `
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="/">Zoekt</a>
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+    <div class="navbar-collapse collapse" id="navbar-collapse" aria-expanded="false" style="height: 1px;">
+      <form class="navbar-form navbar-left" action="search">
+        <div class="form-group">
+          <input class="form-control"
+                placeholder="Search for some code..." role="search"
+                id="navsearchbox" type="text" name="q" autofocus
+                {{if .Query}}
+                value={{.Query}}
+                {{end}}>
+          <div class="input-group">
+            <div class="input-group-addon">Max Results</div>
+            <input class="form-control" type="number" id="maxhits" name="num" value="{{.Num}}">
+          </div>
+          <button class="btn btn-primary">Search</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</nav>
+`,
 	// search box for the entry page.
-	"search": `<html>
-<head>
-<style>
-dt {
-    font-family: monospace;
-}
-</style>
-</head>
+	"search": `
+<html>
+{{template "head"}}
 <title>Zoekt, en gij zult spinazie eten</title>
 <body>
-<div style="margin: 3em; padding 3em; position: center;">
-{{template "searchbox" .Last}}
-</div>
-
-<div style="display: flex; justify-content: space-around; flex-direction: row;">
-
-<div>
-  Search examples:
-  <div style="margin-left: 4em;">
-  <dl>
-    <dt><a href="search?q=needle">needle</a></dt><dd>search for "needle"
-  </dd>
-    <dt><a href="search?q=thread+or+needle">thread or needle</a></dt><dd>search for either "thread" or "needle"
-  </dd>
-    <dt><a href="search?q=class+needle">class needle</a></dt><dd>search for files containing both "class" and "needle"
-  </dd>
-    <dt><a href="search?q=class+Needle">class Needle</a></dt><dd>search for files containing both "class" (case insensitive) and "Needle" (case sensitive)
-  </dd>
-    <dt><a href="search?q=class+Needle+case:yes">class Needle case:yes</a></dt><dd>search for files containing "class" and "Needle", both case sensitively
-  </dd>
-    <dt><a href="search?q=%22class Needle%22">"class Needle"</a></dt><dd>search for files with the phrase "class Needle"
-  </dd>
-     <dt><a href="search?q=needle+-hay">needle -hay</a></dt><dd>search for files with the word "needle" but not the word "hay"
-  </dd>
-    <dt><a href="search?q=path+file:java">path file:java</a></dt><dd>search for the word "path" in files whose name contains "java"
-  </dd>
-    <dt><a href="search?q=needle+lang%3Apython&num=50">needle lang:python</a></dt><dd>search for "needle" in Python source code</dd>
-    <dt><a href="search?q=f:%5C.c%24">f:\.c$</a></dt><dd>search for files whose name ends with ".c"</dd>
-    <dt><a href="search?q=path+-file:java">path -file:java</a></dt><dd>search for the word "path" excluding files whose name contains "java"</dd>
-    <dt><a href="search?q=foo.*bar">foo.*bar</a></dt><dd>search for the regular expression "foo.*bar"</dd>
-    <dt><a href="search?q=-%28Path File%29 Stream">-(Path File) Stream</a></dt><dd>search "Stream", but exclude files containing both "Path" and "File"</dd>
-    <dt><a href="search?q=-Path%5c+file+Stream">-Path\ file Stream</a></dt><dd>search "Stream", but exclude files containing "Path File"</dd>
-    <dt><a href="search?q=phone+r:droid">phone r:droid</a></dt><dd>search for "phone" in repositories whose name contains "droid"</dd>
-    <dt><a href="search?q=phone+b:master">phone b:master</a></dt><dd>for Git repos, find "phone" in files in branches whose name contains "master".</dd>
-    <dt><a href="search?q=phone+b:HEAD">phone b:HEAD</a></dt><dd>for Git repos, find "phone" in the default ('HEAD') branch.</dd>
-  </dl>
+  <div class="jumbotron">
+    <div class="container">
+      {{template "searchbox" .Last}}
+    </div>
   </div>
-</div>
 
-<div>
-
-<p>
-Used {{HumanUnit .Stats.IndexBytes}} memory for
-{{.Stats.Documents}} documents ({{HumanUnit .Stats.ContentBytes}})
-from {{.Stats.Repos}} repositories.
-
-
-<p>
-To list repositories, try:
-  <div style="margin-left: 4em;">
-  <dl>
-    <dt><a href="search?q=r:droid">r:droid</a></dt><dd>list repositories whose name contains "droid".</dd>
-    <dt><a href="search?q=r:go+-r:google">r:go -r:google</a></dt><dd>list repositories whose name contains "go" but not "google".</dd>
-  </dl>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8">
+        <h3>Search examples:</h3>
+        <dl class="dl-horizontal">
+          <dt><a href="search?q=needle">needle</a></dt><dd>search for "needle"</dd>
+          <dt><a href="search?q=thread+or+needle">thread or needle</a></dt><dd>search for either "thread" or "needle"</dd>
+          <dt><a href="search?q=class+needle">class needle</a></span></dt><dd>search for files containing both "class" and "needle"</dd>
+          <dt><a href="search?q=class+Needle">class Needle</a></dt><dd>search for files containing both "class" (case insensitive) and "Needle" (case sensitive)</dd>
+          <dt><a href="search?q=class+Needle+case:yes">class Needle case:yes</a></dt><dd>search for files containing "class" and "Needle", both case sensitively</dd>
+          <dt><a href="search?q=%22class Needle%22">"class Needle"</a></dt><dd>search for files with the phrase "class Needle"</dd>
+          <dt><a href="search?q=needle+-hay">needle -hay</a></dt><dd>search for files with the word "needle" but not the word "hay"</dd>
+          <dt><a href="search?q=path+file:java">path file:java</a></dt><dd>search for the word "path" in files whose name contains "java"</dd>
+          <dt><a href="search?q=needle+lang%3Apython&num=50">needle lang:python</a></dt><dd>search for "needle" in Python source code</dd>
+          <dt><a href="search?q=f:%5C.c%24">f:\.c$</a></dt><dd>search for files whose name ends with ".c"</dd>
+          <dt><a href="search?q=path+-file:java">path -file:java</a></dt><dd>search for the word "path" excluding files whose name contains "java"</dd>
+          <dt><a href="search?q=foo.*bar">foo.*bar</a></dt><dd>search for the regular expression "foo.*bar"</dd>
+          <dt><a href="search?q=-%28Path File%29 Stream">-(Path File) Stream</a></dt><dd>search "Stream", but exclude files containing both "Path" and "File"</dd>
+          <dt><a href="search?q=-Path%5c+file+Stream">-Path\ file Stream</a></dt><dd>search "Stream", but exclude files containing "Path File"</dd>
+          <dt><a href="search?q=phone+r:droid">phone r:droid</a></dt><dd>search for "phone" in repositories whose name contains "droid"</dd>
+          <dt><a href="search?q=phone+b:master">phone b:master</a></dt><dd>for Git repos, find "phone" in files in branches whose name contains "master".</dd>
+          <dt><a href="search?q=phone+b:HEAD">phone b:HEAD</a></dt><dd>for Git repos, find "phone" in the default ('HEAD') branch.</dd>
+        </dl>
+      </div>
+      <div class="col-md-4">
+        <h3>To list repositories, try:</h3>
+        <dl class="dl-horizontal">
+          <dt><a href="search?q=r:droid">r:droid</a></dt><dd>list repositories whose name contains "droid".</dd>
+          <dt><a href="search?q=r:go+-r:google">r:go -r:google</a></dt><dd>list repositories whose name contains "go" but not "google".</dd>
+        </dl>
+      </div>
+    </div>
   </div>
-</p>
-</div>
-</div>
-
-<hr>
-
-<div>
-<a href="about">About</a>
-</div>
-
+  <nav class="navbar navbar-default navbar-fixed-bottom">
+    <div class="container">
+      <a class="navbar-text" href="about">About</a>
+      <p class="navbar-text navbar-right">
+        Used {{HumanUnit .Stats.IndexBytes}} mem for
+        {{.Stats.Documents}} documents ({{HumanUnit .Stats.ContentBytes}})
+        from {{.Stats.Repos}} repositories.
+      </p>
+    </div>
+  </nav>
 </body>
 </html>
 `,
 
-	"results": `<html>
-  <head>
-    <title>Results for {{.QueryStr}}</title>
-  </head>
-<body>
-  {{template "searchbox" .Last}}
-<hr>
-  {{if .Stats.Crashes}}<br><b>{{.Stats.Crashes}} shards crashed</b><br>{{end}}
-  Found {{.Stats.MatchCount}} results in {{.Stats.FileCount}} files
-    ({{HumanUnit .Stats.IndexBytesLoaded}}B index data,
-     {{.Stats.NgramMatches}} ngram matches,
-     {{.Stats.FilesConsidered}} docs considered,
-     {{.Stats.FilesLoaded}} docs ({{HumanUnit .Stats.ContentBytesLoaded}}B) loaded,
-     {{.Stats.FilesSkipped}} docs skipped): for
-  <pre style="background: #ffc;">{{.Query}} with options {{.SearchOptions}}</pre>
-  in {{.Stats.Duration}} (queued: {{.Stats.Wait}})
-  <p>
-  {{range .FileMatches}}
-
-    {{if .URL}}<a name="{{.ResultID}}" href="{{.URL}}" >{{else}}<a name="{{.ResultID}}">{{end}}
-    <tt><b>{{.Repo}}</b>:<b>{{.FileName}}</b></a>:{{if .Branches}}<small>[{{range .Branches}}{{.}}, {{end}}] {{if .Language}}<i>lang:{{.Language}}</i>{{end}}</small>{{end}} </tt>
-      {{if .DuplicateID}}
-        <div style="background: #eff"><a href="#{{.DuplicateID}}"><pre>duplicate result</pre></a></div>
-      {{else}}
-        <div style="background: #eef;">
+	"results": `
+<html>
+{{template "head"}}
+<title>Results for {{.QueryStr}}</title>
+<body id="results">
+  {{template "navbar" .Last}}
+  <div class="container-fluid">
+    <h5>
+      {{if .Stats.Crashes}}<br><b>{{.Stats.Crashes}} shards crashed</b><br>{{end}}
+      Found {{.Stats.MatchCount}} results in {{.Stats.FileCount}} files
+    </h5>
+    {{range .FileMatches}}
+    <table class="table table-hover table-condensed">
+      <thead>
+        <tr>
+          <th>
+            {{if .URL}}<a name="{{.ResultID}}" href="{{.URL}}" >{{else}}<a name="{{.ResultID}}">{{end}}
+            <small>
+              {{.Repo}}:{{.FileName}}</a>:
+              [{{if .Branches}}
+              {{range .Branches}}<span class="label label-default">{{.}}</span>,
+              {{end}}]
+              {{if .Language}}<i>lang:{{.Language}}</i>{{end}}{{end}}
+              {{if .DuplicateID}}
+              <a href="#{{.DuplicateID}}"><pre>duplicate result</pre></a></div>
+              {{end}}
+            </small>
+          </th>
+        </tr>
+      </thead>
+      {{if not .DuplicateID}}
+      <tbody>
         {{range .Matches}}
-          <pre>{{if .URL}}<a href="{{.URL}}">{{end}}{{.LineNum}}{{if .URL}}</a>{{end}}: {{range .Fragments}}{{.Pre}}<b>{{.Match}}</b>{{.Post}}{{end}}</pre>
+        <tr>
+          <td style="background-color: rgba(238, 238, 255, 0.6);">
+            <pre class="inline-pre">{{if .URL}}<a href="{{.URL}}">{{end}}<u>{{.LineNum}}</u>{{if .URL}}</a>{{end}}: {{range .Fragments}}{{.Pre}}<b>{{.Match}}</b>{{.Post}}{{end}}</pre>
+          </td>
+        </tr>
         {{end}}
-        </div>
+      </tbody>
       {{end}}
-  {{end}}
+    </table>
+    {{end}}
+    <hr>
+    <p class="text-right">
+      {{HumanUnit .Stats.IndexBytesLoaded}}B index data,
+      {{.Stats.NgramMatches}} ngram matches,
+      {{.Stats.FilesConsidered}} docs considered,
+      {{.Stats.FilesLoaded}} docs ({{HumanUnit .Stats.ContentBytesLoaded}}B) loaded,
+      {{.Stats.FilesSkipped}} docs skipped
+    </p>
+    <!--samp>{{.Query}} with options {{.SearchOptions}} in {{.Stats.Duration}} (queued: {{.Stats.Wait}})</samp-->
+  </div>
+  {{ template "jsdep"}}
 </body>
 </html>
 `,
 
-	"repolist": `<html>
-  <head>
-    <title>Repo search result for {{.Last.Query}}</title>
-  </head>
-<body>
-{{template "searchbox" .Last}}
- <hr>
-  Found {{.Stats.Repos}} repositories ({{.Stats.Documents}} files, {{HumanUnit .Stats.ContentBytes}}b content):
-  <p>
-  {{range .Repos}}
-    <li>
-      <tt>{{if .URL}}<a href="{{.URL}}">{{end}}{{.Name}}{{if .URL}}</a>{{end}}
-      </tt> (<small>{{.IndexTime.Format "Jan 02, 2006 15:04"}}</small>). Branches:
-      {{range .Branches}}
-         {{if .URL}}<a href="{{.URL}}">{{end}}{{.Name}}{{if .URL}}</a>{{end}},
+	"repolist": `
+<html>
+{{template "head"}}
+<body id="results">
+  <div class="container">
+    {{template "navbar" .Last}}
+    <table class="table table-hover table-condensed">
+    <thead>
+      <tr>
+        <th>Found {{.Stats.Repos}} repositories ({{.Stats.Documents}} files, {{HumanUnit .Stats.ContentBytes}}b content)</th>
+        <th>Last updated</th>
+        <th>Branches</th>
+        <th>Size</th>
+      </tr>
+    </thead>
+    <tbody>
+      {{range .Repos}}
+      <tr>
+        <td>{{if .URL}}<a href="{{.URL}}">{{end}}{{.Name}}{{if .URL}}</a>{{end}}</td>
+        <td><small>{{.IndexTime.Format "Jan 02, 2006 15:04"}}</small></td>
+        <td style="vertical-align: middle;">
+          {{range .Branches}}
+          {{if .URL}}<tt><a class="label label-default small" href="{{.URL}}">{{end}}{{.Name}}{{if .URL}}</a> </tt>{{end}}&nbsp;
+          {{end}}
+        </td>
+        <td><small>{{HumanUnit .Files}} files ({{HumanUnit .Size}})</small></td>
+      </tr>
       {{end}}
-      <small>{{HumanUnit .Files}} files ({{HumanUnit .Size}})</small>
-   </li>
-  {{end}}
-  </ul>
+    </tbody>
+    </ul>
+  </div>
+  {{ template "jsdep"}}
 </body>
 </html>
 `,
@@ -211,9 +282,9 @@ To list repositories, try:
 `,
 
 	"about": `
-  <head>
-    <title>About <em>zoekt</em></title>
-  </head>
+<head>
+  <title>About <em>zoekt</em></title>
+</head>
 <body>
 
 <p>
