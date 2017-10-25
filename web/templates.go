576b4981391c27236c03909177daf30e8ad199a5
Han-Wen Nienhuys <hanwen@google.com>
2017-10-25 22:06:17 +0200

    web: restore host customization and autofocus in box on opening page
    Change-Id: I9be2c8a3d58308dbfd79ece6735d6ce03b3bc0ef

--- web/templates.go
+++ web/templates.go
@@ -63,7 +63,10 @@ var TemplateText = map[string]string{
 <form action="search">
   <div class="form-group form-group-lg">
     <div class="input-group input-group-lg">
-      <input class="form-control" placeholder="Search for some code..."
+      <input class="form-control" placeholder="Search for some code..." autofocus
+              {{if .Query}}
+              value={{.Query}}
+              {{end}}
               id="searchbox" type="text" name="q">
       <div class="input-group-btn">
         <button class="btn btn-primary">Search</button>
