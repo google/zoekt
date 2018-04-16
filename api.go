1bc3d43c898d6ec0e9b528724c3e607e65cd8072
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2018-04-16 22:21:47 +0100

    Add canonical import comment

--- api.go
+++ api.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package zoekt
+package zoekt // import "github.com/google/zoekt"
 
 import (
 	"context"
