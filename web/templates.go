e7066dece791cedcd35e21fd075a2059e8d56486
Han-Wen Nienhuys <hanwen@google.com>
2018-05-23 15:19:19 +0200

    web: fix broken </table> in repolist template
    Change-Id: Ifb0760326d741a9e728a1de57825ce65c4e755c3

--- web/templates.go
+++ web/templates.go
@@ -67,7 +67,7 @@ var TemplateText = map[string]string{
   .container-results {
      overflow: auto;
      max-height: calc(100% - 72px);
-  } 
+  }
   .inline-pre {
      border: unset;
      background-color: unset;
@@ -270,29 +270,29 @@ var TemplateText = map[string]string{
     Found {{.Stats.Repos}} repositories ({{.Stats.Documents}} files, {{HumanUnit .Stats.ContentBytes}}b content)
     </b></div>
     <table class="table table-hover table-condensed">
-    <thead>
-      <tr>
-        <th>Name <a href="/search?q={{.Last.Query}}&order=name">▼</a><a href="/search?q={{.Last.Query}}&order=revname">▲</a></th>
-        <th>Last updated <a href="/search?q={{.Last.Query}}&order=revtime">▼</a><a href="/search?q={{.Last.Query}}&order=time">▲</a></th>
-        <th>Branches</th>
-        <th>Size <a href="/search?q={{.Last.Query}}&order=revsize">▼</a><a href="/search?q={{.Last.Query}}&order=size">▲</a></th>
-      </tr>
-    </thead>
-    <tbody>
-      {{range .Repos}}
-      <tr>
-        <td>{{if .URL}}<a href="{{.URL}}">{{end}}{{.Name}}{{if .URL}}</a>{{end}}</td>
-        <td><small>{{.IndexTime.Format "Jan 02, 2006 15:04"}}</small></td>
-        <td style="vertical-align: middle;">
-          {{range .Branches}}
-          {{if .URL}}<tt><a class="label label-default small" href="{{.URL}}">{{end}}{{.Name}}{{if .URL}}</a> </tt>{{end}}&nbsp;
-          {{end}}
-        </td>
-        <td><small>{{HumanUnit .Files}} files ({{HumanUnit .Size}})</small></td>
-      </tr>
-      {{end}}
-    </tbody>
-    </ul>
+      <thead>
+	<tr>
+	  <th>Name <a href="/search?q={{.Last.Query}}&order=name">▼</a><a href="/search?q={{.Last.Query}}&order=revname">▲</a></th>
+	  <th>Last updated <a href="/search?q={{.Last.Query}}&order=revtime">▼</a><a href="/search?q={{.Last.Query}}&order=time">▲</a></th>
+	  <th>Branches</th>
+	  <th>Size <a href="/search?q={{.Last.Query}}&order=revsize">▼</a><a href="/search?q={{.Last.Query}}&order=size">▲</a></th>
+	</tr>
+      </thead>
+      <tbody>
+	{{range .Repos}}
+	<tr>
+	  <td>{{if .URL}}<a href="{{.URL}}">{{end}}{{.Name}}{{if .URL}}</a>{{end}}</td>
+	  <td><small>{{.IndexTime.Format "Jan 02, 2006 15:04"}}</small></td>
+	  <td style="vertical-align: middle;">
+	    {{range .Branches}}
+	    {{if .URL}}<tt><a class="label label-default small" href="{{.URL}}">{{end}}{{.Name}}{{if .URL}}</a> </tt>{{end}}&nbsp;
+	    {{end}}
+	  </td>
+	  <td><small>{{HumanUnit .Files}} files ({{HumanUnit .Size}})</small></td>
+	</tr>
+	{{end}}
+      </tbody>
+    </table>
   </div>
 
   <nav class="navbar navbar-default navbar-bottom">
@@ -327,7 +327,7 @@ var TemplateText = map[string]string{
       </p>
     </div>
   </nav>
-  </div> 
+  </div>
  {{ template "jsdep"}}
 </body>
 </html>
