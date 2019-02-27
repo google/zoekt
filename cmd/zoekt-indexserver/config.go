0be87929233283903f4de88986f05f8970acc6e7
Han-Wen Nienhuys <hanwen@google.com>
2019-02-27 07:41:59 +0100

    cmd/zoekt-indexserver: make ConfigEntry public
    It is an exposed part of the API, so it is effectively public. Make
    the type public so we can use it in testing.
    
    Change-Id: Ia1b178683a8b45a6f3793b9cdb0823ced7ebf3ba

--- cmd/zoekt-indexserver/config.go
+++ cmd/zoekt-indexserver/config.go
@@ -30,7 +30,7 @@ import (
 	"github.com/fsnotify/fsnotify"
 )
 
-type configEntry struct {
+type ConfigEntry struct {
 	GithubUser             string
 	BitBucketServerProject string
 	GitHubURL              string
@@ -43,10 +43,10 @@ type configEntry struct {
 	GitLabURL              string
 }
 
-func randomize(entries []configEntry) []configEntry {
+func randomize(entries []ConfigEntry) []ConfigEntry {
 	perm := rand.Perm(len(entries))
 
-	var shuffled []configEntry
+	var shuffled []ConfigEntry
 	for _, i := range perm {
 		shuffled = append(shuffled, entries[i])
 	}
@@ -59,7 +59,7 @@ func isHTTP(u string) bool {
 	return err == nil && (asURL.Scheme == "http" || asURL.Scheme == "https")
 }
 
-func readConfigURL(u string) ([]configEntry, error) {
+func readConfigURL(u string) ([]ConfigEntry, error) {
 	var body []byte
 	var readErr error
 
@@ -79,7 +79,7 @@ func readConfigURL(u string) ([]configEntry, error) {
 		return nil, readErr
 	}
 
-	var result []configEntry
+	var result []ConfigEntry
 	if err := json.Unmarshal(body, &result); err != nil {
 		return nil, err
 	}
@@ -129,7 +129,7 @@ func periodicMirrorFile(repoDir string, opts *Options, pendingRepos chan<- strin
 		}
 	}
 
-	var lastCfg []configEntry
+	var lastCfg []ConfigEntry
 	for {
 		cfg, err := readConfigURL(opts.mirrorConfigFile)
 		if err != nil {
@@ -148,7 +148,7 @@ func periodicMirrorFile(repoDir string, opts *Options, pendingRepos chan<- strin
 	}
 }
 
-func executeMirror(cfg []configEntry, repoDir string, pendingRepos chan<- string) {
+func executeMirror(cfg []ConfigEntry, repoDir string, pendingRepos chan<- string) {
 	// Randomize the ordering in which we query
 	// things. This is to ensure that quota limits don't
 	// always hit the last one in the list.
