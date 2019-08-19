014a202a7b3d7ee56540503e51aade2325b259b0
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2019-08-19 22:26:17 +0200

    indexserver: More test cases for GetIndexOptions
    Change-Id: I68a059e2ec2c319ec9c354f246e4d633105cadcd

--- cmd/zoekt-sourcegraph-indexserver/main_test.go
+++ cmd/zoekt-sourcegraph-indexserver/main_test.go
@@ -9,23 +9,37 @@ import (
 )
 
 func TestGetIndexOptions(t *testing.T) {
+	var response []byte
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`{"Symbols": true, "LargeFiles": ["foo","bar"]}`))
+		w.Write(response)
 	}))
 	defer server.Close()
 
-	u, _ := url.Parse(server.URL)
-	opts, err := getIndexOptions(u, server.Client())
+	u, err := url.Parse(server.URL)
 	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
+		t.Fatal(err)
 	}
 
-	if len(opts.LargeFiles) == 0 {
-		t.Error("expected non-empty result from large files list")
+	cases := map[string][]string{
+		`{"Symbols": true, "LargeFiles": ["foo","bar"]}`: []string{"-require_ctags", "-large_file", "foo", "-large_file", "bar"},
+
+		`{"Symbols": false, "LargeFiles": ["foo","bar"]}`: []string{"-disable_ctags", "-large_file", "foo", "-large_file", "bar"},
+
+		`{}`: []string{"-disable_ctags"},
+
+		`{"Symbols": true}`: []string{"-require_ctags"},
 	}
 
-	want := []string{"-require_ctags", "-large_file", "foo", "-large_file", "bar"}
-	if got := opts.toArgs(); !reflect.DeepEqual(got, want) {
-		t.Errorf("got unexpected arguments from options\ngot: %v\nwant: %v\n", got, want)
+	for r, want := range cases {
+		response = []byte(r)
+
+		opts, err := getIndexOptions(u, server.Client())
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+
+		if got := opts.toArgs(); !reflect.DeepEqual(got, want) {
+			t.Errorf("got unexpected arguments from options\ngot: %v\nwant: %v\n", got, want)
+		}
 	}
 }
