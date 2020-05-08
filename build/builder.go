google/zoekt

commit 5dd47134b5c134e3c308c74f13d21dfc7822572f

build: factor out build flags
We currently have all flags related to build options defined in the cmd
package. These flags are as a side-effect to the CommandLine
flagset. This commit factors it out into the build package and allows
setting it on a different flagset.

This is working towards creating command line arguments from a
BuildOptions struct. This can be used by indexserver.

Change-Id: Ibb711b3620d68571325d52f5507f99b7fab234e3

build/builder.go
@@ -18,6 +18,7 @@ package build
 
 import (
 	"crypto/sha1"
+	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -93,6 +94,29 @@ func (o *Options) HashOptions() string {
 	hasher.Write([]byte(fmt.Sprintf("%q", o.LargeFiles)))
 
 	return fmt.Sprintf("%x", hasher.Sum(nil))
+}
+
+type largeFilesFlag struct{ *Options }
+
+func (f largeFilesFlag) String() string {
+	s := append([]string{""}, f.LargeFiles...)
+	return strings.Join(s, "-large_file ")
+}
+
+func (f largeFilesFlag) Set(value string) error {
+	f.LargeFiles = append(f.LargeFiles, value)
+	return nil
+}
+
+// Flags adds flags for build options to fs.
+func (o *Options) Flags(fs *flag.FlagSet) {
+	fs.IntVar(&o.SizeMax, "file_limit", 2<<20, "maximum file size")
+	fs.IntVar(&o.TrigramMax, "max_trigram_count", 20000, "maximum number of trigrams per document")
+	fs.IntVar(&o.ShardMax, "shard_limit", 100<<20, "maximum corpus size for a shard")
+	fs.IntVar(&o.Parallelism, "parallelism", 4, "maximum number of parallel indexing processes.")
+	fs.StringVar(&o.IndexDir, "index", DefaultDir, "directory for search indices")
+	fs.BoolVar(&o.CTagsMustSucceed, "require_ctags", false, "If set, ctags calls must succeed.")
+	fs.Var(largeFilesFlag{o}, "large_file", "A glob pattern where matching files are to be index regardless of their size. You can add multiple patterns by setting this more than once.")
 }
 
 // Builder manages (parallel) creation of uniformly sized shards. The

cmd/flags.go
@@ -19,37 +19,18 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/google/zoekt"
 	"github.com/google/zoekt/build"
 )
 
-type largeFilesFlag []string
-
-func (f *largeFilesFlag) String() string {
-	s := append([]string{""}, *f...)
-	return strings.Join(s, "-large_file ")
-}
-
-func (f *largeFilesFlag) Set(value string) error {
-	*f = append(*f, value)
-	return nil
-}
-
 var (
-	sizeMax     = flag.Int("file_limit", 2<<20, "maximum file size")
-	trigramMax  = flag.Int("max_trigram_count", 20000, "maximum number of trigrams per document")
-	shardLimit  = flag.Int("shard_limit", 100<<20, "maximum corpus size for a shard")
-	parallelism = flag.Int("parallelism", 4, "maximum number of parallel indexing processes.")
-	indexDir    = flag.String("index", build.DefaultDir, "directory for search indices")
-	version     = flag.Bool("version", false, "Print version number")
-	ctags       = flag.Bool("require_ctags", false, "If set, ctags calls must succeed.")
-	largeFiles  = largeFilesFlag{}
+	version = flag.Bool("version", false, "Print version number")
+	opts    = &build.Options{}
 )
 
 func init() {
-	flag.Var(&largeFiles, "large_file", "A glob pattern where matching files are to be index regardless of their size. You can add multiple patterns by setting this more than once.")
+	opts.Flags(flag.CommandLine)
 }
 
 func OptionsFromFlags() *build.Options {
@@ -59,15 +40,6 @@ func OptionsFromFlags() *build.Options {
 		os.Exit(0)
 	}
 
-	opts := &build.Options{
-		Parallelism:      *parallelism,
-		SizeMax:          *sizeMax,
-		ShardMax:         *shardLimit,
-		IndexDir:         *indexDir,
-		CTagsMustSucceed: *ctags,
-		LargeFiles:       largeFiles,
-		TrigramMax:       *trigramMax,
-	}
 	opts.SetDefaults()
 	return opts
 }

commit b6829cd649950a83cb1c0fcd08f9524242e569bf

build: handle nil Options in largeFilesFlag
The flag package may call the String method with a zero-valued receiver,
such as a nil pointer. For example this happens when passing in the flag
"-help", so this fixes a panic in that case. This was a panic introduced
in 5dd4713 build: factor out build flags.

Change-Id: I27e5b66637fe2f3ad8231baa3e5b355b579e052f

build/builder.go
@@ -100,6 +100,13 @@ func (o *Options) HashOptions() string {
 type largeFilesFlag struct{ *Options }
 
 func (f largeFilesFlag) String() string {
+	// From flag.Value documentation:
+	//
+	// The flag package may call the String method with a zero-valued receiver,
+	// such as a nil pointer.
+	if f.Options == nil {
+		return ""
+	}
 	s := append([]string{""}, f.LargeFiles...)
 	return strings.Join(s, "-large_file ")
 }