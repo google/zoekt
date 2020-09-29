d0d95b84fe019f98f4b6986416468eb447ac05d6
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2020-09-29 11:44:55 +0200

    build: always run checktext
    Previously we elided the CheckText check if a filename passed the
    IgnoreSizeMax test. However, CheckText is still needed to ensure we have
    well formed UTF-8 data. There are also some other checks. The builder
    depends on these checks. For example with invalid utf-8 data there is an
    infinite loop in the index builder.
    
    This commit will always run CheckText. However, it removes the trigram
    max check in the case of large files.
    
    Change-Id: I95c1f452ae28ccacadcc299b896ba3c1a5fca17d

--- build/builder.go
+++ build/builder.go
@@ -23,6 +23,7 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"math"
 	"net/url"
 	"os"
 	"os/exec"
@@ -351,17 +352,23 @@ func (b *Builder) AddFile(name string, content []byte) error {
 }
 
 func (b *Builder) Add(doc zoekt.Document) error {
-	// We could pass the document on to the shardbuilder, but if
-	// we pass through a part of the source tree with binary/large
-	// files, the corresponding shard would be mostly empty, so
-	// insert a reason here too.
-	if !b.opts.IgnoreSizeMax(doc.Name) {
-		if len(doc.Content) > b.opts.SizeMax {
-			doc.SkipReason = fmt.Sprintf("document size %d larger than limit %d", len(doc.Content), b.opts.SizeMax)
-		} else if err := zoekt.CheckText(doc.Content, b.opts.TrigramMax); err != nil {
-			doc.SkipReason = err.Error()
-			doc.Language = "binary"
-		}
+	allowLargeFile := b.opts.IgnoreSizeMax(doc.Name)
+
+	// Adjust trigramMax for allowed large files so we don't exclude them.
+	trigramMax := b.opts.TrigramMax
+	if allowLargeFile {
+		trigramMax = math.MaxInt64
+	}
+
+	if len(doc.Content) > b.opts.SizeMax && !allowLargeFile {
+		// We could pass the document on to the shardbuilder, but if
+		// we pass through a part of the source tree with binary/large
+		// files, the corresponding shard would be mostly empty, so
+		// insert a reason here too.
+		doc.SkipReason = fmt.Sprintf("document size %d larger than limit %d", len(doc.Content), b.opts.SizeMax)
+	} else if err := zoekt.CheckText(doc.Content, trigramMax); err != nil {
+		doc.SkipReason = err.Error()
+		doc.Language = "binary"
 	}
 
 	b.todo = append(b.todo, &doc)
