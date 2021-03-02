1e934d0e2521d60fd4f94d8a608781dceb27b76e
Stefan Hengl <stefan@sourcegraph.com>
2021-03-02 13:20:48 +0100

    web: fix operation name for traceAwareSearcher (#73)
    For `traceAwareSearcher`, both `Search` and `StreamingSearch` had the same 
    operation name which was confusing to read in the traces.

--- web/trace.go
+++ web/trace.go
@@ -18,18 +18,18 @@ type traceAwareSearcher struct {
 }
 
 func (s traceAwareSearcher) Search(ctx context.Context, q query.Q, opts *zoekt.SearchOptions) (*zoekt.SearchResult, error) {
-	ctx, finish := getTraceContext(ctx, opts.Trace, opts.SpanContext)
+	ctx, finish := getTraceContext(ctx, "zoekt.traceAwareSearcher.Search", opts.Trace, opts.SpanContext)
 	defer finish()
 	return s.Searcher.Search(ctx, q, opts)
 }
 
 func (s traceAwareSearcher) StreamSearch(ctx context.Context, q query.Q, opts *zoekt.SearchOptions, sender zoekt.Sender) error {
-	ctx, finish := getTraceContext(ctx, opts.Trace, opts.SpanContext)
+	ctx, finish := getTraceContext(ctx, "zoekt.traceAwareSearcher.StreamSearch", opts.Trace, opts.SpanContext)
 	defer finish()
 	return s.Searcher.StreamSearch(ctx, q, opts, sender)
 }
 
-func getTraceContext(ctx context.Context, traceEnabled bool, spanContext map[string]string) (context.Context, func()) {
+func getTraceContext(ctx context.Context, opName string, traceEnabled bool, spanContext map[string]string) (context.Context, func()) {
 	ctx = trace.WithOpenTracingEnabled(ctx, traceEnabled)
 	finish := func() {}
 	if traceEnabled && spanContext != nil {
@@ -38,7 +38,7 @@ func getTraceContext(ctx context.Context, traceEnabled bool, spanContext map[str
 			log.Printf("Error extracting span from opts: %s", err)
 		}
 		if spanContext != nil {
-			span, newCtx := opentracing.StartSpanFromContext(ctx, "zoekt.traceAwareSearcher.Search", opentracing.ChildOf(spanContext))
+			span, newCtx := opentracing.StartSpanFromContext(ctx, opName, opentracing.ChildOf(spanContext))
 			finish = span.Finish
 			ctx = newCtx
 		}
