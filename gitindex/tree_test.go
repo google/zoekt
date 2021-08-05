1b03f8a223fb2637c2071772a3b7d98ecd8ee2b2
Han-Wen Nienhuys <hanwen@google.com>
2021-08-05 11:18:18 +0200

    gitindex: set initial branch explicitly
    Local settings may set the default to "main" for the initial branch,
    leading to spurious test failures.
    
    Change-Id: Ia23b8e837265503b1f0b64982b8b95f7ab7c02aa

--- gitindex/tree_test.go
+++ gitindex/tree_test.go
@@ -39,7 +39,7 @@ func createSubmoduleRepo(dir string) error {
 	}
 	script := `mkdir adir bdir
 cd adir
-git init
+git init -b master
 mkdir subdir
 echo acont > afile
 echo sub-cont > subdir/sub-file
@@ -50,7 +50,7 @@ git commit -am amsg
 
 cd ..
 cd bdir
-git init
+git init -b master
 echo bcont > bfile
 git add bfile
 git config user.email "you@example.com"
@@ -300,7 +300,7 @@ func createMultibranchRepo(dir string) error {
 	}
 	script := `mkdir repo
 cd repo
-git init
+git init -b master
 mkdir subdir
 echo acont > afile
 echo sub-cont > subdir/sub-file
